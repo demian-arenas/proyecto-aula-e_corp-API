126de824775da38ff7623441efe3132367adddaf
Demian Arenas <arenasdemian99@gmail.com>
2017-05-04 00:40:39 -0500

    Creacion de controladores de Home y Administrador. Implementado el registro e inicio de sesion

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+  secret: process.env.E_CORP_SECRET || 'n0m3l0' // never use default
+}
---
+++ home.js
@@ -0,0 +1,67 @@
+'use strict'
+
+import { send, json } from 'micro'
+import HttpHash from 'http-hash'
+import Db from 'e-corp-db'
+import config from './config'
+import utils from './lib/utils'
+
+let db = new Db()
+
+const hash = HttpHash()
+
+hash.set('GET /signup', async function getCountry (req, res, params) {
+  let gender = await db.getGender()
+  let country = await db.getCountry()
+  let dataForm = {
+    paises: country,
+    generos: gender
+  }
+  send(res, 200, dataForm)
+})
+
+hash.set('GET /signup/:var', async function getCountry (req, res, params) {
+  let countryCode = params.var
+  let cityByCountry = await db.getCityByCountry(countryCode)
+  send(res, 200, cityByCountry)
+})
+
+hash.set('POST /signup', async function saveUser (req, res, params) {
+  let credentials = await json(req)
+  await db.saveUserData(credentials)
+  .then(async (rows) => {
+    credentials.idDatos = rows.insertId
+    await db.saveUserAut(credentials)
+    .then((rows) => {
+      send(res, 201)
+    })
+    .catch((err) => send(res, 409, err))
+  })
+  .catch((err) => send(res, 409, err))
+})
+
+hash.set('POST /login', async function loginUser (req, res, params) {
+  let credentials = await json(req)
+  let auth = await db.authenticateUser(credentials.nickname, credentials.password, 1)
+  if (!auth) return send(res, 401, { error: 'invalid credentials' })
+  let token = await utils.signToken({
+    userId: credentials.nickname
+  }, config.secret)
+
+  send(res, 200, token)
+})
+
+export default async function main (req, res) {
+  let { method, url } = req
+  let match = hash.get(`${method.toUpperCase()} ${url}`)
+
+  if (match.handler) {
+    try {
+      await match.handler(req, res, match.params)
+    } catch (e) {
+      send(res, 500, { error: e.message })
+    }
+  } else {
+    send(res, 404, { error: 'route not found' })
+  }
+}
